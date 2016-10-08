d1d0d784fcaacb51d920730e5cfed25508cac051
Yinghao Xu <Yinghao Xu>
2016-10-08 05:40:52 +0200

    Added end and close

--- app.js
+++ app.js
@@ -13,11 +13,38 @@ bot.onText(/\/echo/, function(msg) {
 
 bot.onText(/\/start/, function(msg) {
     var fromId = msg.chat.id;
-    var game = {
-        'players': [],
-        'join': true,
-        'tokyo': undefined
-    };
-    games[fromId] = game;
-    bot.sendMessage(fromId, 'GAME STARTED!.\nJoin the game typing /join in the chat.\nStart playing typing /close.');
+    if (games[fromId] !== undefined) {
+        bot.sendMessage(fromId,'Game already started');
+    }
+    else {
+        var game = {
+            'players': [],
+            'join': true,
+            'tokyo': undefined
+        };
+        games[fromId] = game;
+        bot.sendMessage(fromId, 'GAME STARTED!.\nJoin the game typing /join in the chat.\nStart playing typing /close.');
+    }
+});
+
+bot.onText(/\/close/, function(msg) {
+    var fromId = msg.chat.id;
+    if (games[fromId].join === true) {
+        games[fromId].join = false;
+        bot.sendMessage(fromId, 'No more players are accepted.');
+    }
+    else {
+        bot.sendMessage(fromId, 'Game already started');
+    }
+});
+
+bot.onText(/\/end/, function(msg) {
+    var fromId = msg.chat.id;
+    if (games[fromId] !== undefined) {
+        delete games[fromId];
+        bot.sendMessage(fromId,'Ending game...');
+    }
+    else {
+        bot.sendMessage(fromId, 'No games running.');
+    }
 });
