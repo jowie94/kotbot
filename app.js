c7b3b1ee0115a95663a604d34b00415a12eb9e1e
Yinghao Xu <Yinghao Xu>
2016-10-08 09:13:45 +0200

    Roll the dice!

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ var bot = new TelegramBot(token, {polling: true});
 
 var games = {};
 
+var GameStates = {
+    JOINING: 0,
+    PREROLL: 1,
+    DICESELECT: 2
+}
+
+var emojis = [
+
+]
+
+
 bot.onText(/\/echo/, function(msg) {
     var fromId = msg.chat.id;
     bot.sendMessage(fromId, "Hello world!");
@@ -20,7 +31,8 @@ bot.onText(/\/start/, function(msg) {
         var game = {
             'players': [],
             'join': true,
-            'tokyo': undefined
+            'tokyo': undefined,
+            'state': GameStates.JOINING
         };
         games[fromId] = game;
         bot.sendMessage(fromId, 'GAME STARTED!.\nJoin the game typing /join in the chat.\nStart playing typing /close.');
@@ -29,9 +41,14 @@ bot.onText(/\/start/, function(msg) {
 
 bot.onText(/\/close/, function(msg) {
     var fromId = msg.chat.id;
-    if (games[fromId].join === true) {
-        games[fromId].join = false;
-        bot.sendMessage(fromId, 'No more players are accepted.');
+    var game = games[fromId];
+    if (game.join === true) {
+        game.join = false;
+        //bot.sendMessage(fromId, 'No more players are accepted.');
+        game.currentPlayer = game.players[0].id;
+        bot.sendMessage(fromId, 'No more players are accepted.\nPlayer @' + game.players[0].name + ' starts.');
+        //bot.sendMessage(fromId, 'Player @' + game.players[0].name + ' starts');
+        beginRollDice(fromId, game.players[0], game);
     }
     else {
         bot.sendMessage(fromId, 'Game already started');
@@ -75,3 +92,56 @@ bot.onText(/\/join/, function(msg) {
         bot.sendMessage(fromId, 'Player @' + msg.from.username + ' joined');
     }
 });
+
+bot.onText(/Roll the dice/, function(msg) {
+    var fromId = msg.chat.id;
+    if (!gameIsRunning(fromId)) {
+        bot.sendMessage(fromId, 'There is no game running on this chat!');
+        return;
+    }
+
+    var game = games[fromId];
+    if (game.currentPlayer !== msg.from.id) {
+        bot.sendMessage(fromId, 'It isn\'t your turn', {'reply_to_message_id': msg.message_id});
+        return;
+    }
+
+    if (game.state !== GameStates.PREROLL) {
+        bot.sendMessage(fromId, 'You can\'t roll the dices now', {'reply_to_message_id': msg.message_id});
+        return;
+    }
+
+    var result = rollDices(6);
+    var str = '';
+    result.forEach((value, index) => str += (index + 1) + '. ' + value + '\n');
+    bot.sendMessage(fromId, 'Result:\n' + str);
+    game.state = GameStates.DICESELECT;
+});
+
+function gameIsRunning(gameId) {
+    return gameIsStarted(gameId) && !games[gameId].join;
+}
+
+function gameIsStarted(gameId) {
+    return games[gameId] !== undefined;
+}
+
+function beginRollDice(chatId, player, game) {
+    game.state = GameStates.PREROLL;
+    var keyboard = {
+        'keyboard': [['Roll the dice']],
+        'resize_keyboard': true,
+        'one_time_keyboard': true,
+        'selective': true
+    };
+    bot.sendMessage(chatId, '@' + player.name + ' roll the dice, please', {'reply_markup': keyboard});
+}
+
+function rollDices(amount) {
+    var dices = [];
+    for (i = 0; i < amount; i++) { 
+        dices[i] = Math.floor(Math.random() * 11)%6;
+        console.log(dices[i]);
+    }
+    return dices;
+}
