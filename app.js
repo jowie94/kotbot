7130c54b37950670a6a537bb55876c061b5a6850
Yinghao Xu <Yinghao Xu>
2016-10-08 04:51:26 +0200

    Added start command

--- app.js
+++ app.js
@@ -4,7 +4,20 @@ var token = process.env.bot_key;
 
 var bot = new TelegramBot(token, {polling: true});
 
+var games = {};
+
 bot.onText(/\/echo/, function(msg) {
     var fromId = msg.chat.id;
     bot.sendMessage(fromId, "Hello world!");
+});
+
+bot.onText(/\/start/, function(msg) {
+    var fromId = msg.chat.id;
+    var game = {
+        'players': [],
+        'join': true,
+        'tokyo': undefined
+    };
+    games[fromId] = game;
+    bot.sendMessage(fromId, 'GAME STARTED!.\nJoin the game typing /join in the chat.\nStart playing typing /close.');
 });
