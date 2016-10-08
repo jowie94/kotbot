df9c956c08e8e6df29e5b486ef3fe005b66faedf
Joel Lopez <jowie.jowie@gmail.com>
2016-10-08 04:16:51 +0200

    Missing changes

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 var TelegramBot = require('node-telegram-bot-api');
 
+var token = process.env.bot_key;
 
+var bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/echo/, function(msg) {
+    var fromId = msg.chat.id;
+    bot.sendMessage(fromId, "Hello world!");
+});
