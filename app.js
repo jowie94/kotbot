35ecdb3f792ff681d63aae1d819e19565e2741f8
Joel Lopez <jowie.jowie@gmail.com>
2016-10-09 02:31:26 +0200

    Workaround for heroku

--- app.js
+++ app.js
@@ -498,3 +498,22 @@ function createStats(player) {
 }
 
 console.log('Server started!');
+
+// Workaround for heroku
+var http = require('http');
+
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+var PORT = process.env.PORT || 3000;
+
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
